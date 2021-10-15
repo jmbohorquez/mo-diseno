53e37b2ee127dc7bc4acb71f57a221b9b02b6c3e
Julian Bohorquez <jm.bohorquez@gmail.com>
2021-10-15 16:11:06 -0500

    agregar horarios y contacto

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,9 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>MO Diseño y Arquitectura</h1>
         <p>Especialistas en servicio de construcción y remodelaciones en Colombia</p>
+        <p><strong>Telefono:</strong> +57 (300) 791-8979</p>
+        <p><strong>Email:</strong> gerencia@modisenoyarquitectura.com</p>
+        <p><strong>Horario:</strong> Lunes a viernes: 07:00 - 17:00 hrs</p>
       </main>
     </div>
   )
